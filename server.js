785f228199841305d71f285db6262a544926cdb4
함수바나나 <cwd50013@naver.com>
2026-03-26 18:10:22 +0900

    Refactor static file handling and add main page route
    Updated static file serving and added main page route.

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
+// 정적 파일 폴더
+app.use(express.static(path.join(__dirname, "public")));
 
+// 메인 페이지 강제 연결 (Cannot GET / 해결)
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// 소켓 연결
 io.on("connection", (socket) => {
+    console.log("유저 접속");
+
     socket.on("chat message", (msg) => {
         io.emit("chat message", msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("유저 나감");
     });
 });
 
